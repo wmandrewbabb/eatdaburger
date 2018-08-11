79ab1076b501df058ac84e3a94caefced885f199
WMABABB <wm.andrew.babb@gmail.com>
2018-08-11 17:15:17 -0400

    Fixing Jaws

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,8 @@ CREATE TABLE burgers
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN NOT NULL DEFAULT 0,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
 	PRIMARY KEY (id)
 );
+
+-- timestamp apparently needed for JawsDB to make things easier since I'm having difficulty editing it manually through the workbench
